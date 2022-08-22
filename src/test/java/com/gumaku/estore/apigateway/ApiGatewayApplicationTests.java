954e7a44fcc2415f15c96f8663c2110896fdfd9d
TANG KIN PAN <tkp@TANGs-MacBook-Pro.local>
2022-08-23 00:42:18 +0800

    first commit

---
+++ src/test/java/com/gumaku/estore/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumaku.estore.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
